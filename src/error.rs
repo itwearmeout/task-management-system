509ed5317ab8e1f35744aebb062fbf344c0c27f1
itwearmeout <crysero2@gmail.com>
2026-02-12 23:26:37 +0700

    implementation for error

--- src/error.rs
+++ src/error.rs
@@ -27,7 +27,7 @@ pub enum Error {
     NotFound,
 
     #[error("error in the request body")]
-    UnprocessableEntry {
+    UnprocessableEntity {
         errors: Hashmap<Cow<'static, str>, Vec<Cow<'static,str>>>,
     },
 
@@ -36,5 +36,34 @@ pub enum Error {
 
     #[error("an internal server error occurred")]
     Anyhow(#[from] anyhow::error),
+}
+
+impl Error {
+    pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
+    where
+        K: Into<Cow<'static, str>>,
+        V: Intor<Cow<'static, str>>
+    {
+        let mut error_map = Hashmap::new();
+
+        for(key, val) in errors {
+            error_map
+                .entry(key.into())
+                .or_insert_with(Vec::new)
+                .push(val.into())
+        }
+
+        Self::UnprocessableEntity { errors: error_map }
+    }
+
+    pub fn status_code(&self) -> StatusCode {
+        match self {
+            Self::Unautherize => StatusCode::UNAUTHERIZE,
+            Self::Forbidden => StatusCode::FORBIDDEN,
+            Self::NotFound => StatusCode::NOT_FOUND,
+            Self::UnprocessableEntity{ .. } => StatusCode::UNPROCESSABLE_ENTITY,
+            Self::Sqlx(_) | Self::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
 
