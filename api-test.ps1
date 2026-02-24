49dff3214894daf539a927cebdbd1030f0ed476f
Andrew Jack <crysero2@gmail.com>
2026-02-24 23:09:12 +0700

    auth test

---
+++ api-test.ps1
@@ -0,0 +1,36 @@
+// Route test
+curl http://localhost:3000/task
+
+// Route GET linux
+curl -X GET http://localhost:3000/api/task/get
+
+//Route GET Windows
+irm http://localhost:3000/api/task/get
+
+//Route POST (Create) users
+$body = @{
+    user = @{
+        username = "test"
+        email = "test@test"
+        password = "test"
+        angkatan = 2024
+    }
+} | ConvertTo-Json
+
+Invoke-WebRequest -Uri "http://localhost:3000/api/users/create" `
+    -Method POST `
+    -Body $body `
+    -ContentType "application/json"
+
+//Route POST (Login) users 
+$body = @{
+    user = @{
+        username = "test"
+        password = "test"
+    }
+} | ConvertTo-Json
+
+Invoke-WebRequest -Uri "http://localhost:3000/api/users/login" `
+    -Method POST `
+    -Body $body `
+    -ContentType "application/json"
